524e8e326e419e6c7c0d4320eacbe2fec143caac
ProfGoldstein <igoldstein@siena.edu>
2024-01-18 21:46:52 -0500

    Initial upload

---
+++ EveryNth.java
@@ -0,0 +1,27 @@
+/*
+* Skeletal code for in-class problem 1
+* CSIS225  Spring 2024
+*/
+
+import java.util.*;
+
+public class EveryNth {
+    public static void main(String[] args) {
+		
+		// Check command line argumemts
+		if ( ) {
+			System.out.println(" ");
+			System.exit(1);
+		}
+		
+		// Create an input scanner from the standard input
+
+		// Get the input
+
+		// Break it into words
+		
+		// Display every nth word
+    }
+}
+
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# CSIS225
+# In Class Problem 1
+# Spring 2024
